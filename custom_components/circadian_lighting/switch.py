7f87ab2d848998db53def63c9d63dc812b8a67be
Mark Suurmond <mark.suurmond@onegini.coml>
2020-03-24 21:37:19 +0100

    removed the max brightness during the day

--- custom_components/circadian_lighting/switch.py
+++ custom_components/circadian_lighting/switch.py
@@ -243,10 +243,7 @@ class CircadianSwitch(SwitchDevice, RestoreEntity):
                 _LOGGER.debug(self._name + " in Sleep mode")
                 return self._sleep_brightness
             else:
-                if self._cl.data['percent'] > 0:
-                    return self._max_brightness
-                else:
-                    return ((self._max_brightness - self._min_brightness) * ((100+self._cl.data['percent']) / 100)) + self._min_brightness
+                return ((self._max_brightness - self._min_brightness) * ((100+self._cl.data['percent']) / 100)) + self._min_brightness
 
     def update_switch(self, transition=None):
         if self._cl.data is not None:
