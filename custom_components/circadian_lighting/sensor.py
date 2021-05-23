2d90e19e5515a145bbea8175fff274eda7749b65
Robert <robertomano24@gmail.com>
2021-05-23 16:01:29 -0400

    Return to Original CL
    This sensor has been returned to the original function of circadian lighting. Since CL has not been updated for a little over a year, this fork will continue to maintain compatibility with HA as HA continues to be updated. You can use this fork for CL functionality with HA. If you prefer superior Hue integration with CL, please check out this repo: https://github.com/robertomano24/circadian_lighting-hue

--- custom_components/circadian_lighting/sensor.py
+++ custom_components/circadian_lighting/sensor.py
@@ -4,50 +4,18 @@ Circadian Lighting Sensor for Home-Assistant.
 
 DEPENDENCIES = ['circadian_lighting']
 
-
 import logging
-import re
-import requests
-import json
-import math
-from datetime import timedelta
 
 from custom_components.circadian_lighting import DOMAIN, CIRCADIAN_LIGHTING_UPDATE_TOPIC, DATA_CIRCADIAN_LIGHTING
 
 from homeassistant.helpers.dispatcher import dispatcher_connect
 from homeassistant.helpers.entity import Entity
-from homeassistant.util.color import (
-    color_RGB_to_xy, color_temperature_kelvin_to_mired,
-    color_temperature_to_rgb, color_xy_to_hs)
+
 import datetime
 
 _LOGGER = logging.getLogger(__name__)
 
 ICON = 'mdi:theme-light-dark'
-####################################
-hue_gateway = "INPUTHUEIPHERE"
-key = "INPUTHUEAPIKEYHERE"
-####################################
-def update_scene_lights(scene, brightness, x_val, y_val, mired):
-	url = "http://" + hue_gateway + "/api/" + key + "/scenes/" + scene + "/"
-	r = requests.get(url).json()
-	r = r['lights']
-	_LOGGER.debug("Updating scene id:" + str(scene))
-	for val in r:
-		url = "http://" + hue_gateway + "/api/" + key + "/lights/" + str(val)
-		t = requests.get(url).json()
-		type = t['type']
-		url = "http://" + hue_gateway + "/api/" + key + "/scenes/" + scene + "/lightstates/" + str(val)
-		if type == 'Color temperature light':
-			body = json.dumps({'on': True, 'bri': brightness, 'ct': mired})
-		if type == 'Extended color light':
-			body = json.dumps({'on': True, 'bri': brightness, 'ct': mired})
-		if type == 'Dimmable light':
-			body = json.dumps({'on': True, 'bri': brightness})
-		r = requests.put(url, data=body)
-		_LOGGER.debug("light id: " + str(val) + " body " + str(body) + " status code: " + str(r.status_code))
-		if int(r.status_code) != int(200):
-			_LOGGER.error("light id: " + str(val) + " body" + str(body) + " status code: " + str(r.status_code))
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Set up the Circadian Lighting sensor."""
@@ -117,7 +85,7 @@ class CircadianSensor(Entity):
     @property
     def device_state_attributes(self):
         """Return the attributes of the sensor."""
-        return dict((k,str(v) if isinstance(v, datetime.time) or isinstance(v, datetime.timedelta) else v) for k,v in self._attributes.items())
+        return self._attributes
 
     def update(self):
         """Fetch new state data for the sensor.
@@ -126,31 +94,11 @@ class CircadianSensor(Entity):
         """
         self._cl.update()
 
-
     def update_sensor(self):
         if self._cl.data is not None:
             self._state = self._cl.data['percent']
             self._hs_color = self._cl.data['hs_color']
-#            self._attributes = self._cl.data
             self._attributes['colortemp'] = self._cl.data['colortemp']
             self._attributes['rgb_color'] = self._cl.data['rgb_color']
             self._attributes['xy_color'] = self._cl.data['xy_color']
-	    min_brightness = 30
-	    max_brightness = 100
-	    brightness = int(((max_brightness - min_brightness) * ((100+self._cl.data['percent']) / 100)) + (min_brightness / 100) * 254)
-            ct = color_temperature_kelvin_to_mired(self._cl.data['colortemp'])
-            rgb = color_temperature_to_rgb(self._cl.data['colortemp'])
-	    _LOGGER.debug("RGB values: " + str(rgb))
-            xy = color_RGB_to_xy(rgb[0],rgb[1],rgb[2])
-
-            url = "http://" + hue_gateway + "/api/" + key + "/scenes/"
-	    r = requests.get(url).json()
-
-	    scenes = []
-            for val in r:
-		name = r[val]['name']
-		if re.match(r"Circadian", name):
-		    scenes.append(val)
-
-	    for val in scenes:
-		update_scene_lights(val, brightness, xy[0], xy[1], ct)
+            _LOGGER.debug("Circadian Lighting Sensor Updated")
